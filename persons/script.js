dee156cdffc31a972d1dae0e0f070a25dcffc3cd
mk-franke <mk-franke@mail.ru>
2020-05-19 20:32:30 +0200

    #53 HW 2020-05-15

--- persons/script.js
+++ persons/script.js
@@ -6,21 +6,42 @@ function showUsersTable(persons = []) {
 
     let averageAge = 0;
     let innerHtmlTableBody = '';
-        for (let i = 0; i < persons.length; i++) {
-            innerHtmlTableBody += '<tr><td>' + sortedPersons[i].name + '</td><td>' + sortedPersons[i].age + '</td></tr>';
-            console.log('Person ' + (i + 1) + ': name = ' + sortedPersons[i].name + ' age = ' + sortedPersons[i].age);
-            averageAge += + sortedPersons[i].age;
-        }
+    for (let i = 0; i < persons.length; i++) {
+        innerHtmlTableBody += '<tr><td>' + sortedPersons[i].firstName + '</td><td>' + sortedPersons[i].lastName + '</td><td>' + sortedPersons[i].age + '</td></tr>';
+        console.log('Person ' + (i + 1) + ': firstName = ' + sortedPersons[i].firstName + ' firstName = ' + sortedPersons[i].firstName + ' age = ' + sortedPersons[i].age);
+        averageAge += + sortedPersons[i].age;
+    }
     averageAge /= persons.length;
+
+    let maxAge = 0;
+    for (let i = 0; i < persons.length; i++) {
+        if (persons[i].age > maxAge) {
+            maxAge = persons[i].age;
+        }
+    }
+
+    let minAge = 150;
+    for (let i = 0; i < persons.length; i++) {
+        if (persons[i].age < minAge) {
+            minAge = persons[i].age;
+        }
+    }
 
     document.getElementsByTagName('tbody')[0].innerHTML = innerHtmlTableBody;
 
     console.log('Average age of person:' + averageAge);
     document.querySelector('tfoot div.alert').innerHTML = 'Average age: ' + averageAge;
+
+    console.log('Max age of person:' + maxAge);
+    document.getElementById("max").innerHTML = 'Max age: ' + maxAge;
+
+    console.log('Min age of person:' + minAge);
+    document.getElementById("min").innerHTML = 'Min age: ' + minAge;
+
 }
 
 let persons = [];
-let averageAge = 0;
+
 const personsNumber = prompt('Please enter the number of persons:');
 console.log(personsNumber);
 
@@ -28,7 +49,8 @@ console.log(personsNumber);
 for (let i = 0; i < personsNumber; i++) {
     setTimeout(function() {
         const person = {};
-        person.name = prompt('Please enter name of the ' + (i + 1) + ' person');
+        person.firstName = prompt('Please enter First name of the ' + (i + 1) + ' person');
+        person.lastName = prompt('Please enter Last name of the ' + (i + 1) + ' person');
         person.age = prompt('Please enter age of the ' + (i + 1) + 'person');
         persons[i] = person;
 
