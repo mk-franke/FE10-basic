790b363bb12f5b288da9057662f770f44e436dfb
isalexlog <--global>
2020-06-26 19:44:52 +0200

    HW 2020-06-25 - paragraphs solution with closure (more informative output to console)

--- paragraphs/script.js
+++ paragraphs/script.js
@@ -2,15 +2,17 @@ const colors = ['white', 'red', 'green', 'blue'];
 //                 0       1       2        3  
 
 
-function createEventListener() {
+function createEventListener(i) {
     let currentColor = 0;
     return function() {
         currentColor++;
+        console.log("Click on " + i + " paragraph, currentColor = " + currentColor);
         this.style['background-color'] = colors[currentColor % colors.length];
     }
 }
 
-document.querySelectorAll('p').forEach(function(element) {
-  element.addEventListener('click', createEventListener());
+document.querySelectorAll('p').forEach(function(element, i) {
+    console.log(i);
+    element.addEventListener('click', createEventListener(i));
 })
 
