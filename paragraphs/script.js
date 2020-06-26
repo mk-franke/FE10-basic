87b39fb4fedbfdb7b2b1fe133dab22ac1c18877b
isalexlog <--global>
2020-06-26 19:17:38 +0200

    HW 2020-06-25 - fixing paragraphs project with closure

--- paragraphs/script.js
+++ paragraphs/script.js
@@ -1,11 +1,16 @@
 const colors = ['white', 'red', 'green', 'blue'];
 //                 0       1       2        3  
 
-let currentColor = 0;
-document.querySelectorAll('p').forEach(function(element) {
-    element.addEventListener('click', function() {
+
+function createEventListener() {
+    let currentColor = 0;
+    return function() {
         currentColor++;
         this.style['background-color'] = colors[currentColor % colors.length];
-    });
+    }
+}
+
+document.querySelectorAll('p').forEach(function(element) {
+  element.addEventListener('click', createEventListener());
 })
 
